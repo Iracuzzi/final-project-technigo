105e00995fafbefd9da450107c6ca3765c249351
Iracuzzi <arnau.monforte@gmail.com>
2023-01-23 14:06:02 +0100

    asd

--- frontend/src/components/CharacterList.js
+++ frontend/src/components/CharacterList.js
@@ -1,7 +1,21 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styled, { css } from 'styled-components'
+import { API_URL } from "utils/utils";
 
 const CharacterList = () => {
+  const [characterList, setCharacterList] = useState([])
+  const [mode, setMode] = useState("character-list");
+  const fetchCharacters = () => {
+    fetch(API_URL(mode))
+      .then(res => res.json())
+      .then(data => setCharacterList(data))
+      .catch(error => console.log(error))
+  }
+  useEffect(() => {
+    if (!accessToken) {
+        navigate("/login");
+    }
+}, [accessToken])
 
   return (
     <>
